08cb0e25566b7d5bc474f34ad5adf45bfa2e2bb7
meet-mobilefirst <meetprajapati.mobilefirst@gmail.com>
2021-02-20 14:26:20 +0530

    Added login for doctor and patient. Added home page for patient

---
+++ lib/ui/doctor/doctor_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DoctorHomePage extends StatefulWidget {
+  @override
+  _DoctorHomePageState createState() => _DoctorHomePageState();
+}
+
+class _DoctorHomePageState extends State<DoctorHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
