1283b349f3bda85710f5eab41962f72e7ad87eba
meetcse <ymba.meet@gmail.com>
2021-04-17 00:53:25 +0530

    Added splash screen

--- lib/ui/splash_screen/splash_screen.dart
+++ lib/ui/splash_screen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:pcare/Utils/PageUtils.dart';
 import 'package:pcare/api/login/login_api.dart';
 import 'package:pcare/constants/preferences.dart';
@@ -23,8 +24,14 @@ class _SplashScreenState extends State<SplashScreen> {
   LoginController controller = Get.find<LoginController>();
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    changeScreen();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    changeScreen(context);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       extendBody: false,
@@ -52,19 +59,47 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
+  List<Color> colorizeColors = [
+    Colors.purple,
+    Colors.blue,
+    Colors.yellow,
+    Colors.red,
+  ];
   Widget _buildSplashImage() {
-    return Container(
-      height: MediaQuery.of(Get.context).size.height,
-      width: MediaQuery.of(Get.context).size.width,
-      child: Image(
-        image: AssetImage("assets/splash/splash.png"),
-        fit: BoxFit.fill,
-      ),
+    return Stack(
+      // mainAxisAlignment: MainAxisAlignment.center,
+      // crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Positioned(
+          bottom: 0,
+          right: 0,
+          left: -740,
+          child: Container(
+            // height: MediaQuery.of(Get.context).size.height,
+            // width: MediaQuery.of(Get.context).size.width,
+            child: Center(
+              child: Lottie.asset(
+                'assets/splash/splash_video.json',
+                height: Get.height,
+                width: Get.width,
+                fit: BoxFit.cover,
+                repeat: true,
+                options: LottieOptions(enableMergePaths: true),
+                animate: true,
+              ),
+            ),
+            // Image(
+            //   image: AssetImage("assets/splash/splash.png"),
+            //   fit: BoxFit.fill,
+            // ),
+          ),
+        ),
+      ],
     );
   }
 
-  changeScreen(BuildContext context) {
-    Future.delayed(Duration(seconds: 3), () async {
+  changeScreen() {
+    Future.delayed(Duration(seconds: 5), () async {
       _isLoading = true;
       if (mounted) setState(() {});
       if (_sfService.getBoolFromPref(Preferences.isLoggedIn) == null ||
